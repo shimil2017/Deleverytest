3560babd26965ab768a0f4761b4b9f2f720bd084
HoneySachdeva <mahesh.kumar@smartdatainc.net>
2017-09-20 12:26:32 +0530

    Added Travelers list class

---
+++ src/components/TravelersList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { List, ListItem } from 'react-native-elements';
+import { View,Text,KeyboardAvoidingView,ActivityIndicator, Dimensions } from 'react-native';
+
+export default class TravevelersList extends Component {
+  constructor() {
+
+  }
+}
